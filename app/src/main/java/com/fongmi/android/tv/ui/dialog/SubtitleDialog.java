58df9ef82065c441dae887d538eb06d351b0b432
wobuhui666 <121330915+wobuhui666@users.noreply.github.com>
2025-04-01 21:16:25 +0800

    更新 SubtitleDialog.java

--- app/src/main/java/com/fongmi/android/tv/ui/dialog/SubtitleDialog.java
+++ app/src/main/java/com/fongmi/android/tv/ui/dialog/SubtitleDialog.java
@@ -1,5 +1,7 @@
 package com.fongmi.android.tv.ui.dialog;
 
+import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,6 +26,11 @@ public final class SubtitleDialog extends BaseDialog {
     private SubtitleView subtitleView;
     private boolean full;
 
+    private float bottomPaddingFraction = SubtitleView.DEFAULT_BOTTOM_PADDING_FRACTION;
+    private float textSize = 1.0f; // 初始大小，可以根据需求调整
+    private static final float PADDING_INCREMENT = 0.005f;
+    private static final float TEXT_SIZE_INCREMENT = 0.002f;
+
     public static SubtitleDialog create() {
         return new SubtitleDialog();
     }
@@ -39,7 +46,8 @@ public final class SubtitleDialog extends BaseDialog {
     }
 
     public void show(FragmentActivity activity) {
-        for (Fragment f : activity.getSupportFragmentManager().getFragments()) if (f instanceof BottomSheetDialogFragment) return;
+        for (Fragment f : activity.getSupportFragmentManager().getFragments())
+            if (f instanceof BottomSheetDialogFragment) return;
         show(activity.getSupportFragmentManager(), null);
     }
 
@@ -56,7 +64,17 @@ public final class SubtitleDialog extends BaseDialog {
     @Override
     protected void initView() {
         int count = binding.getRoot().getChildCount();
-        if (full) for (int i = 0; i < count; i++) ((ImageView) binding.getRoot().getChildAt(i)).getDrawable().setTint(MDColor.WHITE);
+        if (full)
+            for (int i = 0; i < count; i++)
+                ((ImageView) binding.getRoot().getChildAt(i)).getDrawable().setTint(MDColor.WHITE);
+
+        int savedPadding = Setting.getSubtitleBottomPadding();
+        bottomPaddingFraction = savedPadding / 1000.0f;
+        subtitleView.setBottomPaddingFraction(bottomPaddingFraction);
+
+        int savedTextSize = Setting.getSubtitleTextSize();
+        textSize = savedTextSize / 1000.0f;
+        subtitleView.setTextSize(androidx.media3.common.util.Util.TEXT_SIZE_UNIT_EM, textSize);
     }
 
     @Override
@@ -69,23 +87,29 @@ public final class SubtitleDialog extends BaseDialog {
     }
 
     private void onUp(View view) {
-        subtitleView.addBottomPadding(0.005f);
-        Setting.putSubtitleBottomPadding(subtitleView.getBottomPadding());
+        bottomPaddingFraction += PADDING_INCREMENT;
+        subtitleView.setBottomPaddingFraction(bottomPaddingFraction);
+        Setting.putSubtitleBottomPadding((int) (bottomPaddingFraction * 1000));
     }
 
     private void onDown(View view) {
-        subtitleView.subBottomPadding(0.005f);
-        Setting.putSubtitleBottomPadding(subtitleView.getBottomPadding());
+        bottomPaddingFraction -= PADDING_INCREMENT;
+        if (bottomPaddingFraction < 0) bottomPaddingFraction = 0;
+        subtitleView.setBottomPaddingFraction(bottomPaddingFraction);
+        Setting.putSubtitleBottomPadding((int) (bottomPaddingFraction * 1000));
     }
 
     private void onLarge(View view) {
-        subtitleView.addTextSize(0.002f);
-        Setting.putSubtitleTextSize(subtitleView.getTextSize());
+        textSize += TEXT_SIZE_INCREMENT;
+        subtitleView.setTextSize(androidx.media3.common.util.Util.TEXT_SIZE_UNIT_EM, textSize);
+        Setting.putSubtitleTextSize((int) (textSize * 1000));
     }
 
     private void onSmall(View view) {
-        subtitleView.subTextSize(0.002f);
-        Setting.putSubtitleTextSize(subtitleView.getTextSize());
+        textSize -= TEXT_SIZE_INCREMENT;
+        if (textSize < 0.5f) textSize = 0.5f;
+        subtitleView.setTextSize(androidx.media3.common.util.Util.TEXT_SIZE_UNIT_EM, textSize);
+        Setting.putSubtitleTextSize((int) (textSize * 1000));
     }
 
     private void onReset(View view) {
@@ -93,6 +117,8 @@ public final class SubtitleDialog extends BaseDialog {
         Setting.putSubtitleBottomPadding(0);
         subtitleView.setUserDefaultTextSize();
         subtitleView.setBottomPaddingFraction(SubtitleView.DEFAULT_BOTTOM_PADDING_FRACTION);
+        bottomPaddingFraction = SubtitleView.DEFAULT_BOTTOM_PADDING_FRACTION;
+        textSize = 1.0f;
     }
 
     @Override
