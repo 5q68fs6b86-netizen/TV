b4f7defca8ff1962328ab5bf78c1f48c7b064c71
wobuhui666 <121330915+wobuhui666@users.noreply.github.com>
2025-04-01 22:15:59 +0800

    更新 SubtitleDialog.java

--- app/src/main/java/com/fongmi/android/tv/ui/dialog/SubtitleDialog.java
+++ app/src/main/java/com/fongmi/android/tv/ui/dialog/SubtitleDialog.java
@@ -1,130 +1,32 @@
-package com.fongmi.android.tv.ui.dialog;
-
-import android.os.Bundle;
 import android.util.TypedValue;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
-import androidx.media3.ui.SubtitleView;
-import androidx.viewbinding.ViewBinding;
+// 修正 initView 方法
+@Override
+protected void initView() {
+    int count = binding.getRoot().getChildCount();
+    if (full)
+        for (int i = 0; i < count; i++)
+            ((ImageView) binding.getRoot().getChildAt(i)).getDrawable().setTint(MDColor.WHITE);
 
-import com.fongmi.android.tv.Setting;
-import com.fongmi.android.tv.databinding.DialogSubtitleBinding;
-import com.fongmi.android.tv.utils.ResUtil;
-import com.github.bassaer.library.MDColor;
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+    float savedPadding = Setting.getSubtitleBottomPadding() / 1000.0f;
+    bottomPaddingFraction = savedPadding;
+    subtitleView.setBottomPaddingFraction(bottomPaddingFraction);
 
-public final class SubtitleDialog extends BaseDialog {
+    float savedTextSize = Setting.getSubtitleTextSize() / 1000.0f;
+    textSize = savedTextSize;
+    subtitleView.setTextSize(TypedValue.COMPLEX_UNIT_SP, textSize);
+}
 
-    private DialogSubtitleBinding binding;
-    private SubtitleView subtitleView;
-    private boolean full;
+// 修正 onLarge 和 onSmall 方法
+private void onLarge(View view) {
+    textSize += TEXT_SIZE_INCREMENT;
+    subtitleView.setTextSize(TypedValue.COMPLEX_UNIT_SP, textSize);
+    Setting.putSubtitleTextSize((int) (textSize * 1000));
+}
 
-    private float bottomPaddingFraction = SubtitleView.DEFAULT_BOTTOM_PADDING_FRACTION;
-    private float textSize = 1.0f; // 初始大小，可以根据需求调整
-    private static final float PADDING_INCREMENT = 0.005f;
-    private static final float TEXT_SIZE_INCREMENT = 0.002f;
-
-    public static SubtitleDialog create() {
-        return new SubtitleDialog();
-    }
-
-    public SubtitleDialog view(SubtitleView subtitleView) {
-        this.subtitleView = subtitleView;
-        return this;
-    }
-
-    public SubtitleDialog full(boolean full) {
-        this.full = full;
-        return this;
-    }
-
-    public void show(FragmentActivity activity) {
-        for (Fragment f : activity.getSupportFragmentManager().getFragments())
-            if (f instanceof BottomSheetDialogFragment) return;
-        show(activity.getSupportFragmentManager(), null);
-    }
-
-    @Override
-    protected boolean transparent() {
-        return full;
-    }
-
-    @Override
-    protected ViewBinding getBinding(@NonNull LayoutInflater inflater, @Nullable ViewGroup container) {
-        return binding = DialogSubtitleBinding.inflate(inflater, container, false);
-    }
-
-    @Override
-    protected void initView() {
-        int count = binding.getRoot().getChildCount();
-        if (full)
-            for (int i = 0; i < count; i++)
-                ((ImageView) binding.getRoot().getChildAt(i)).getDrawable().setTint(MDColor.WHITE);
-
-        int savedPadding = Setting.getSubtitleBottomPadding();
-        bottomPaddingFraction = savedPadding / 1000.0f;
-        subtitleView.setBottomPaddingFraction(bottomPaddingFraction);
-
-        int savedTextSize = Setting.getSubtitleTextSize();
-        textSize = savedTextSize / 1000.0f;
-        subtitleView.setTextSize(androidx.media3.common.util.Util.TEXT_SIZE_UNIT_EM, textSize);
-    }
-
-    @Override
-    protected void initEvent() {
-        binding.up.setOnClickListener(this::onUp);
-        binding.down.setOnClickListener(this::onDown);
-        binding.large.setOnClickListener(this::onLarge);
-        binding.small.setOnClickListener(this::onSmall);
-        binding.reset.setOnClickListener(this::onReset);
-    }
-
-    private void onUp(View view) {
-        bottomPaddingFraction += PADDING_INCREMENT;
-        subtitleView.setBottomPaddingFraction(bottomPaddingFraction);
-        Setting.putSubtitleBottomPadding((int) (bottomPaddingFraction * 1000));
-    }
-
-    private void onDown(View view) {
-        bottomPaddingFraction -= PADDING_INCREMENT;
-        if (bottomPaddingFraction < 0) bottomPaddingFraction = 0;
-        subtitleView.setBottomPaddingFraction(bottomPaddingFraction);
-        Setting.putSubtitleBottomPadding((int) (bottomPaddingFraction * 1000));
-    }
-
-    private void onLarge(View view) {
-        textSize += TEXT_SIZE_INCREMENT;
-        subtitleView.setTextSize(androidx.media3.common.util.Util.TEXT_SIZE_UNIT_EM, textSize);
-        Setting.putSubtitleTextSize((int) (textSize * 1000));
-    }
-
-    private void onSmall(View view) {
-        textSize -= TEXT_SIZE_INCREMENT;
-        if (textSize < 0.5f) textSize = 0.5f;
-        subtitleView.setTextSize(androidx.media3.common.util.Util.TEXT_SIZE_UNIT_EM, textSize);
-        Setting.putSubtitleTextSize((int) (textSize * 1000));
-    }
-
-    private void onReset(View view) {
-        Setting.putSubtitleTextSize(0);
-        Setting.putSubtitleBottomPadding(0);
-        subtitleView.setUserDefaultTextSize();
-        subtitleView.setBottomPaddingFraction(SubtitleView.DEFAULT_BOTTOM_PADDING_FRACTION);
-        bottomPaddingFraction = SubtitleView.DEFAULT_BOTTOM_PADDING_FRACTION;
-        textSize = 1.0f;
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (full) setDimAmount(0.5f);
-        getDialog().getWindow().setLayout(ResUtil.dp2px(248), -1);
-    }
+private void onSmall(View view) {
+    textSize -= TEXT_SIZE_INCREMENT;
+    textSize = Math.max(textSize, 0.5f); // 确保最小值为 0.5
+    subtitleView.setTextSize(TypedValue.COMPLEX_UNIT_SP, textSize);
+    Setting.putSubtitleTextSize((int) (textSize * 1000));
 }
