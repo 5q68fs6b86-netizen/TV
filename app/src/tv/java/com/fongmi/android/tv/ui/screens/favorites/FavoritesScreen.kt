5dd47090eeff22e4a6a59208a73324227d300361
5q68fs6b86-netizen <5q68fs6b86@privaterelay.appleid.com>
2026-02-01 10:56:32 +0000

    feat: 添加 TvColors 主题支持到 FavoritesScreen

--- app/src/tv/java/com/fongmi/android/tv/ui/screens/favorites/FavoritesScreen.kt
+++ app/src/tv/java/com/fongmi/android/tv/ui/screens/favorites/FavoritesScreen.kt
@@ -42,6 +42,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
 import com.fongmi.android.tv.bean.Keep
 import com.fongmi.android.tv.ui.components.FocusableItem
+import com.fongmi.android.tv.ui.theme.TvColors
 import com.fongmi.android.tv.ui.theme.TvDimens
 import com.fongmi.android.tv.ui.theme.TvTypography
 import com.fongmi.android.tv.ui.viewmodel.FavoritesViewModel
