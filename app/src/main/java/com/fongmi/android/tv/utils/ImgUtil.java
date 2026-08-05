d42411794caf71ec6513a3086c90f087cc4eb54b
5q68fs6b86-netizen <5q68fs6b86@privaterelay.appleid.com>
2026-08-05 11:45:39 +0800

    fix(build): remove duplicate image clear method

--- app/src/main/java/com/fongmi/android/tv/utils/ImgUtil.java
+++ app/src/main/java/com/fongmi/android/tv/utils/ImgUtil.java
@@ -93,13 +93,6 @@ public class ImgUtil {
         load(text, url, view, true, false, true, callback);
     }
 
-    public static void clear(ImageView view) {
-        Glide.with(App.get()).clear(view);
-    }
-
-    public static void clear(ImageView view) {
-        Glide.with(App.get()).clear(view);
-    }
 
     public static void load(String text, String url, ImageView view, boolean vod) {
         load(text, url, view, vod, false, false, null);
