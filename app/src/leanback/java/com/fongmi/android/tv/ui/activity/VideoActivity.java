99f03ae3f414a228576d926594d97fefd38b1d49
5q68fs6b86-netizen <5q68fs6b86-netizen@users.noreply.github.com>
2026-08-10 23:47:12 +0800

    fix(playback): handle end state consistently

--- app/src/leanback/java/com/fongmi/android/tv/ui/activity/VideoActivity.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/activity/VideoActivity.java
@@ -1892,8 +1892,8 @@ public class VideoActivity extends PlaybackActivity implements VodPlaybackHost, 
 
     @Override
     public void onSeekEnd(long time) {
+        if (seekTo(time)) hideCenter();
         mKeyDown.reset();
-        seekTo(time);
     }
 
     @Override
