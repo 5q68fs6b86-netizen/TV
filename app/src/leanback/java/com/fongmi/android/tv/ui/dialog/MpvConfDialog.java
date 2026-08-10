2612bc3f5629e8653847d5837dc620f0b3d3e421
5q68fs6b86-netizen <5q68fs6b86-netizen@users.noreply.github.com>
2026-08-10 23:47:12 +0800

    fix(server): open QR codes on settings tab

--- app/src/leanback/java/com/fongmi/android/tv/ui/dialog/MpvConfDialog.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/dialog/MpvConfDialog.java
@@ -39,7 +39,7 @@ public class MpvConfDialog extends BaseAlertDialog {
     @Override
     protected void initView() {
         setText(MpvConfigFiles.read());
-        binding.code.setImageBitmap(QRCode.getJetStreamBitmap(Server.get().getAddress(3), 200, 0));
+        binding.code.setImageBitmap(QRCode.getJetStreamBitmap(Server.get().getAddress(4), 200, 0));
         binding.text.post(() -> {
             if (binding.text.isShown() && binding.text.isEnabled()) binding.text.requestFocus();
         });
